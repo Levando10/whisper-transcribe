2523a4139227fc106d7197614b79abee8d8fc022
dolevan055@gmail.com <dolevan055@gmail.com>
2025-07-06 15:55:33 +0700

    aaz

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import whisper
 import requests
 import tempfile
+import os
 
 app = Flask(__name__)
 model = whisper.load_model("tiny")
@@ -20,4 +21,5 @@ def transcribe():
     return jsonify({"transcript": result["text"]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
